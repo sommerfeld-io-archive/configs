5c7095d11ed06eaa1beba8c3d05d6156331c066a
sebastian <sebastian@sommerfeld.io>
2022-12-15 17:22:03 +0100

    feat: add example secret (no sensitive data) - #16

--- src/main/github/sebastian-sommerfeld-io-trashbox.tf
+++ src/main/github/sebastian-sommerfeld-io-trashbox.tf
@@ -4,3 +4,9 @@
 data "github_repository" "trashbox" {
   full_name = "sebastian-sommerfeld-io/trashbox"
 }
+
+resource "github_actions_secret" "trashbox_example" {
+  repository      = data.github_repository.trashbox.id
+  secret_name     = "EXAMPLE_FROM_TERRAFORM"
+  plaintext_value = "demo"
+}
