d43527f4b4cab9862c811eb21c602a77c0830c45
sebastian <sebastian@sommerfeld.io>
2023-01-10 08:58:07 +0100

    fixed path to file

--- src/main/github/modules/docker-pipelines/workflows.tf
+++ src/main/github/modules/docker-pipelines/workflows.tf
@@ -12,7 +12,7 @@ resource "github_repository_file" "docker_pipeline_security_scans" {
   commit_message      = "[Actions Bot] Update Github Actions workflow"
   overwrite_on_create = true
   file                = ".github/workflows/security-scans.yml"
-  content = templatefile("${path.module}/assets/workflows/websitesecurity-scans-tftpl.yml", {
+  content = templatefile("${path.module}/assets/workflows/security-scans-tftpl.yml", {
     image = "${var.image_name}:${var.image_tag}",
     args  = "--file=${var.dockerfile}"
   })
