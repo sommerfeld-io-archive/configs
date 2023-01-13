78782e919181b23a5b0ef430d803b980aa34d518
sebastian <sebastian@sommerfeld.io>
2023-01-13 11:33:15 +0100

    added DoR and DoD to user story template - resolve #60

--- src/main/github/modules/issues/templates.tf
+++ src/main/github/modules/issues/templates.tf
@@ -4,22 +4,7 @@ resource "github_repository_file" "template_user_story" {
   commit_message      = "[Actions Bot] Template update"
   overwrite_on_create = true
   file                = ".github/ISSUE_TEMPLATE/user-story.md"
-  content             = <<-EOT
-    ---
-    name: User Story
-    about: Describe a new feature (only one single feature at best)
-    title: ''
-    labels: ${resource.github_issue_label.story.name}
-    assignees: ''
-    ---
-
-    As a _role_ I can _capability_, so that _receive benefit_.
-
-    ### Acceptance Criteria
-    - [ ] Describe what criteria the user story must meet.
-    - [ ] Be precise! 
-    - [ ] You should be able to create a dedicated implementation task (and an automated test case at best) for each criteria.
-  EOT
+  content             = file("${path.module}/assets/templates/user-story.md")
 }
 
 resource "github_repository_file" "template_pull_request" {
@@ -28,16 +13,5 @@ resource "github_repository_file" "template_pull_request" {
   commit_message      = "[Actions Bot] Template update"
   overwrite_on_create = true
   file                = ".github/PULL_REQUEST_TEMPLATE.md"
-  content             = <<-EOT
-    # Description
-    Please include a summary of the change and which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change.
-
-    Link Github Issues if possible.
-
-    ## Type of change
-    Please delete options that are not relevant.
-    - [ ] Bug fix (non-breaking change which fixes an issue)
-    - [ ] New feature (non-breaking change which adds functionality)
-    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
-  EOT
+  content             = file("${path.module}/assets/templates/PULL_REQUEST_TEMPLATE.md")
 }
