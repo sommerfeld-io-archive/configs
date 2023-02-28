d72dac594959f3ec3ad42551a1944f9c7049111d
sebastian <sebastian@sommerfeld.io>
2023-02-28 21:00:13 +0100

    conkyrc file no longer hardcoded

--- src/main/homelab/conky/run.sh
+++ src/main/homelab/conky/run.sh
@@ -2,30 +2,27 @@
 # @file provision.sh
 # @brief Run Conky instances on kobol laptop.
 #
-# @description The script starts all Conky instances. Normally this script is triggered automatically
-# when the machine starts (the script is configured as a startup application by ansible playbook).
+# @description The script starts all Conky instances. By default this script is triggered
+# automatically when the machine starts (the script is configured as a startup application
+# by ansible playbook).
 #
 # === Script Arguments
 #
 # The script does not accept any parameters.
 
 
-readonly CONKY_PATH="$HOME/work/repos/sebastian-sommerfeld-io/configs/src/main/homelab/conky"
+readonly BASE_PATH="$HOME/work/repos/sebastian-sommerfeld-io/configs/src/main/homelab/conky"
+readonly CONKYRC_PATH="$BASE_PATH/conkyrc-$HOSTNAME"
 
 
 echo -e "$LOG_INFO Copy launcher to autostart"
 cp "assets/conky-launcher.desktop" "$HOME/.config/autostart/conky-launcher.desktop"
 
 echo -e "$LOG_INFO Start all conky instances in background"
-sleep 120
+sleep 2
 
-readonly conkyDefinitions=(
-  "$CONKY_PATH/config-$HOSTNAME/.conkyrc"
-  "$CONKY_PATH/config-$HOSTNAME/.conkyrc_services"
-)
-for def in "${conkyDefinitions[@]}"
-do
-  echo -e "$LOG_INFO Start Instance $def"
-  sleep 10
-  conky -c "$def" &
+for rc in "$CONKYRC_PATH"/*.conkyrc; do
+  echo -e "$LOG_INFO Start Instance ${P}${rc}${D}"
+  sleep 10s
+  conky -c "$rc" &
 done
