c74145d1fe8734a0bde5b73c91f57d43d91de015
sebastian <sebastian@sommerfeld.io>
2023-02-28 17:29:49 +0100

    install steam from ansible

--- src/main/homelab/bootstrap/install-basics.sh
+++ src/main/homelab/bootstrap/install-basics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# @file bootstrap-kobol.sh
+# @file install-basics.sh
 # @brief Run basic provisioning.
 #
 # @description This script runs the basic provisioning. This is a prerequisite to run the ansible steps.
@@ -12,7 +12,7 @@
 #
 # [source, bash]
 # ```
-# curl https://raw.githubusercontent.com/sebastian-sommerfeld-io/configs/main/src/main/homelab/bootstrap/kobol.sh | bash -
+# curl https://raw.githubusercontent.com/sebastian-sommerfeld-io/configs/main/src/main/homelab/bootstrap/install-basics.sh | bash -
 # ```
 
 
