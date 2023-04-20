6e2e3ec04f4e1e415ae7adea53c8194c2a36ea31
sebastian <sebastian@sommerfeld.io>
2023-04-20 22:12:38 +0200

    added new issue template

--- src/main/github/modules/issues/templates.tf
+++ src/main/github/modules/issues/templates.tf
@@ -7,6 +7,15 @@ resource "github_repository_file" "template_user_story" {
   content             = file("${path.module}/assets/templates/user-story.md")
 }
 
+resource "github_repository_file" "template_risk_technical_debt" {
+  repository          = var.repo_name
+  branch              = "main"
+  commit_message      = "[Actions Bot] Template update"
+  overwrite_on_create = true
+  file                = ".github/ISSUE_TEMPLATE/user-story.md"
+  content             = file("${path.module}/assets/templates/risk-technical-debt.md")
+}
+
 resource "github_repository_file" "template_pull_request" {
   repository          = var.repo_name
   branch              = "main"
