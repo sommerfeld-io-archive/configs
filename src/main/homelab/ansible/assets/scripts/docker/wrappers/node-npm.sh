fe46d9ea06e4fb9c882527501dd2c9eba68a6f0a
sebastian <sebastian@sommerfeld.io>
2023-07-24 21:46:45 +0200

    mount .npm folder to avoid permissions errors

--- src/main/homelab/ansible/assets/scripts/docker/wrappers/node-npm.sh
+++ src/main/homelab/ansible/assets/scripts/docker/wrappers/node-npm.sh
@@ -43,6 +43,7 @@ echo -e "$LOG_INFO Run: $cmd" "$@"
 docker run -it --rm \
   --volume /etc/passwd:/etc/passwd:ro \
   --volume /etc/group:/etc/group:ro \
+  --volume "$HOME/.npm:$HOME/.npm" \
   --user "$(id -u):$(id -g)" \
   --volume "$(pwd):$(pwd)" \
   --workdir "$(pwd)" \
