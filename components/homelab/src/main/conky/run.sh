34083bdc27a917042d1bc80430a6ccac1f0eeeff
sebastian <sebastian@sommerfeld.io>
2024-05-06 11:37:22 +0200

    display conky on primary screen

--- components/homelab/src/main/conky/run.sh
+++ components/homelab/src/main/conky/run.sh
@@ -13,13 +13,14 @@
 
 readonly BASE_PATH="$HOME/work/repos/sebastian-sommerfeld-io/configs/components/homelab/src/main/conky"
 readonly CONKYRC_PATH="$BASE_PATH/conkyrc-$HOSTNAME"
-
+#readonly DELAY="2m"
+readonly DELAY="5s"
 
 echo -e "$LOG_INFO Copy launcher to autostart"
 cp "assets/conky-launcher.desktop" "$HOME/.config/autostart/conky-launcher.desktop"
 
-echo -e "$LOG_INFO Start all conky instances in background"
-sleep 2m
+echo -e "$LOG_INFO Start all conky instances in background (sleep $DELAY first)"
+sleep "$DELAY"
 
 for rc in "$CONKYRC_PATH"/*.conkyrc; do
   echo -e "$LOG_INFO Start Instance ${P}${rc}${D}"
